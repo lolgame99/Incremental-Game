bf230cdb300c1842486d0f6a085d1d0fb6b28d88
lolgame99 <lolgame99@gmail.com>
2017-06-27 20:49:05 +0200

    finished rotation animation

--- resources/scripts/spinner.js
+++ resources/scripts/spinner.js
@@ -1,7 +1,28 @@
+var data = {};
+data.cringeStorage = 0;
 $(function() {
+    rotationAni();
+    $(".spinnerImg").click(function(event) {
+
+        data.cringeStorage += 1;
+        $("#cringeStorage").html(data.cringeStorage);
+    });
+
+});
+
+function rotationAni() {
+    var cringeStorOld = 0;
+    var cringeStorNew = 0;
     var angle = 0;
     setInterval(function() {
-        angle += 1;
-        $("#img").rotate(angle);
+        angle = data.cringeStorage;
+        $(".spinnerImg").rotate(angle);
     }, 50);
-});
+
+    setInterval(function() {
+        cringeStorNew = $("#cringeStorage").html();
+        data.cringePerSec = cringeStorNew - cringeStorOld;
+        $("#cringeMeter").html(data.cringePerSec);
+        cringeStorOld = $("#cringeStorage").html();
+    }, 1000);
+}
